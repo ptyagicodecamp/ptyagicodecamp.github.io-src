f148327cd21b4edf68954e1e7658700425e3a961
ptyagi <ptyagicodecamp@gmail.com>
2016-12-12 14:27:39 -0800

    added verbose logging

--- publish-n-push.sh
+++ publish-n-push.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo "publising changes..."
 make publish
+echo "pushing code to source repo..."
 git add .
 git commit -m "$1"
 git push origin master
+echo "pushing generated website to github"
 cd output
 git add .
 git commit -m "$1"
 git push origin master
+cd ..
