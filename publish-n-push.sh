5beb1986ff83933d669035673c4165fe6eebf662
ptyagi <ptyagicodecamp@gmail.com>
2016-12-16 15:25:32 -0800

    updating script

--- publish-n-push.sh
+++ publish-n-push.sh
@@ -13,6 +13,7 @@ git add .
 git commit -m "$1"
 git push origin master
 cd ..
+echo "updating submodule"
 git add .
-git commit -m "$1"
+git commit -m "updating submodule info"
 git push origin master
