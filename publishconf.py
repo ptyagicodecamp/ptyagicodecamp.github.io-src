398267043015cd5351032af00ab511ab4f65f57b
ptyagi <ptyagicodecamp@gmail.com>
2018-11-18 00:04:04 -0800

    theme updated

--- publishconf.py
+++ publishconf.py
@@ -13,8 +13,8 @@ from pelicanconf import *
 SITEURL = 'https://ptyagicodecamp.github.io'
 RELATIVE_URLS = True
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+#FEED_ALL_ATOM = 'feeds/all.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = False
 
