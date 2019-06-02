2e522c5080c96d6c7ea75e2e7b7bda00a9c7d3b4
ptyagi <ptyagicodecamp@gmail.com>
2019-06-02 02:24:27 -0700

    publishing new content

--- publishconf.py
+++ publishconf.py
@@ -1,13 +1,26 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
 
 import os
 import sys
-
 sys.path.append(os.curdir)
-
 from pelicanconf import *
 
-SITEURL = 'https://ptyagicodecamp.github.io'
+# If your site is available via HTTPS, make sure SITEURL begins with https://
+SITEURL = 'https://ptyagicodecamp.github.io/'
 RELATIVE_URLS = False
 USE_LESS = False
-GOOGLE_ANALYTICS = 'UA-133714063-1'
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = False
+
+# Following items are often useful when publishing
+
+DISQUS_SITENAME = "techlog"
+GOOGLE_ANALYTICS = "UA-133714063-1"
